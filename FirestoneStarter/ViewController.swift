20e27a0f8cf9c15daf74c51f510ebf4838f90ab1
Matthew Deuschle <mdeuschle@gmail.com>
2019-04-21 14:12:24 -0700

    add pods

---
+++ FirestoneStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestoneStarter
+//
+//  Created by Matt Deuschle on 4/21/19.
+//  Copyright © 2019 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+platform :ios, '9.0'
+
+target 'FirestoneStarter' do
+  use_frameworks!
+
+pod 'Firebase/Core'
+pod 'Firebase/Firestore'
+
+  target 'FirestoneStarterTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FirestoneStarterUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
